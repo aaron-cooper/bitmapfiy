21ac6a2a61edc72870200bd5d8b72983d7d02413
Aaron Cooper <30245107+aaron-cooper@users.noreply.github.com>
2023-05-11 03:30:29 -0400

    class for building a list of bits (bytes)

--- BitMapByteListBuilder.cs
+++ BitMapByteListBuilder.cs
@@ -1,19 +1,23 @@
 class BitMapByteListBuilder {
     private List<byte> _bytes = new List<byte>();
     private int _count = 0;
-    private int _numBitsInCurrentByte => _count & 0xff; //_count % 8
-    private int _currByteInd => _count >> 8; //_count / 8
+    private int _numBitsInCurrentByte => _count & 0x7; //_count % 8
+    private int _currByteInd => _count >> 3; //_count / 8
     
 
     public void Append(bool bit){
         if (_currByteInd == _bytes.Count){
             AddNewByte();
         }
-        _bytes[_currByteInd] |= (byte)(bit ? 1 : 0 << _numBitsInCurrentByte);
+        byte mask = (byte)((bit ? 1 : 0) << _numBitsInCurrentByte);
+        _bytes[_currByteInd] |= mask;
         _count++;
     }
 
     public void PadCurrentByte(){
+        if (_numBitsInCurrentByte == 0){
+            return;
+        }
         AddNewByte();
         _count += 8 - _numBitsInCurrentByte;
     }
