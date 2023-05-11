abe80dee56c4921af440f9d94c674451e690d477
Aaron Cooper <30245107+aaron-cooper@users.noreply.github.com>
2023-05-11 03:31:06 -0400

    Class for converting list of bytes to .xbm file

---
+++ XbmBuilder.cs
@@ -0,0 +1,28 @@
+using System.Text;
+using System.Linq;
+
+class XbmBuilder {
+    public string CreateXbm(string name, int width, int height, List<byte> bytes){
+        var string_builder = new StringBuilder();
+        string_builder.AppendLine($"#define {name}_width {width}");
+        string_builder.AppendLine($"#define {name}_height {height}");
+        string_builder.AppendLine($"static unsigned char {name}_bits[] = {{");
+
+        int i = 0;
+        while (i < bytes.Count){
+            string_builder.Append("    ");
+            string_builder.Append($"0x{bytes[i]:x2}");
+            i++;
+            for (int j = i + 11; i < j && i < bytes.Count; i++){
+                string_builder.Append($", 0x{bytes[i]:x2}");
+            }
+            if (i < bytes.Count){
+                string_builder.AppendLine(",");
+            } else {
+                string_builder.AppendLine();
+                string_builder.Append("}");
+            }
+        }
+        return string_builder.ToString();
+    }
+}
