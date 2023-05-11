abe81619ee0ca0688a866fbfd5b82ad8015aa683
Aaron Cooper <30245107+aaron-cooper@users.noreply.github.com>
2023-05-11 00:39:21 -0400

    initial commit
    open a 2 color (black and white) png and open and print it in console

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using SFML.Graphics;
+using System.Text;
+
+var image = new Image("./brick.png");
+var string_builder = new StringBuilder();
+
+for (uint i = 0; i < image.Size.Y; i++){
+    for (uint j = 0; j < image.Size.X; j++){
+        if (image.GetPixel(j, i) == Color.Black){
+            string_builder.Append('#');
+        }
+        else{
+            string_builder.Append(' ');
+        }
+    }
+    string_builder.Append('\n');
+}
+
+Console.Write(string_builder);
